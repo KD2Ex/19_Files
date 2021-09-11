5f8579bda06715a54a81934385f8292d49f62997
KD2Ex <82110046+KD2Ex@users.noreply.github.com>
2021-09-11 12:37:38 +0300

    Task 4 completed

---
+++ Task4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    ifstream file;
+    char buffer[5];
+    string path;
+    cin >> path;
+    file.open(path, ios::binary);
+    file.read(buffer, sizeof(buffer));
+    buffer[4] = 0;
+    if (buffer[0] == (char)-119 && buffer[1] == 'P' && buffer[2] == 'N' && buffer[3] == 'G') {
+        cout << "y";
+    } else {
+        cout << "n";
+    }
+    //cout << buffer << " " << buffer[0] << " " << (char)-119;
+
+}
