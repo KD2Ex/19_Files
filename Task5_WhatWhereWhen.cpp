39a505f0dd6c5d63f150ca5ccd55e610e6e19b6a
KD2Ex <82110046+KD2Ex@users.noreply.github.com>
2021-09-20 21:18:41 +0300

    Add files via upload

--- Task5_WhatWhereWhen.cpp
+++ Task5_WhatWhereWhen.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <windows.h>
 using namespace std;
 
-void printFileContent(ifstream file[]) {
+void printFileContent(ifstream &file) {
+    char buffer[6];
     while (!file.eof()) {
-        string str;
-        file >> str;
-        cout << str;
+        file.read(buffer, sizeof(buffer) - 1);
+        buffer[5] = 0;
+        if (file.gcount() != 5) {
+            for (int  i = file.gcount();  i < 5; ++ i) {
+                buffer[i] = 0;
+            }
+        }
+        cout << buffer;
     }
 }
 
-bool answer(int position, ifstream answer[]) {
-    to_string(position);
-    string answersPath = "W:\\CLionProjects\\Skillbox\\19_Files\\TheGame\\Questions\\"
-            + to_string(position) + ".txt";
-    answer[position].open(answersPath);
+bool answer(ifstream &answer) {
     string rightAnswer;
     string playerAnswer;
-    answer[position] >> rightAnswer;
+    answer >> rightAnswer;
     cin >> playerAnswer;
+
     if (playerAnswer == rightAnswer) {
         return true;
     } else {
@@ -37,32 +41,47 @@ int main() {
     string questionsPath;
     string answersPath;
 
+    for (int i = 0; i < 13; ++i) {
+        sectors[i] = true;
+    }
     //path = "W:\\CLionProjects\\Skillbox\\19_Files\\TheGame\\Questions\\" + to_string(position) + ".txt";
     cout << "The game has begun!" << endl;
-    while (expertsScore != 6 && viewersScore != 6) {
+    while (expertsScore < 6 && viewersScore < 6) {
         cout << "Current position is " << position << endl;
         cout << "Enter the offset:";
         cin >> offset;
-        position = (position + offset) % 13;
-        if (position == 0) position = 13;
+        if (position + offset == 13) {
+            position += offset;
+        } else {
+            position = (position + offset) % 13;
+            if (position == 0) position = 13;
+        }
+
+        while (sectors[position - 1] != true) {
+            ++position;
+        }
 
         questionsPath = "W:\\CLionProjects\\Skillbox\\19_Files\\TheGame\\Questions\\"
                 + to_string(position) + ".txt";
-        /*answersPath = "W:\\CLionProjects\\Skillbox\\19_Files\\TheGame\\Questions\\"
-                + to_string(position) + ".txt";*/
+        answersPath = "W:\\CLionProjects\\Skillbox\\19_Files\\TheGame\\Answers\\"
+                + to_string(position) + ".txt";
 
-        questions[position].open(questionsPath);
-        //answers[position].open(answersPath);
+        questions[position - 1].open(questionsPath);
+        answers[position - 1].open(answersPath);
 
-        printFileContent(questions);
+        printFileContent(questions[position]);
 
         cout << "Enter your answer:\n";
 
-        if (answer(position, answers)) {
+        if (answer(answers[position])) {
             ++expertsScore;
         } else {
             ++viewersScore;
         }
+        cout << "Current state is " << expertsScore << ":" << viewersScore << endl;
+        sectors[position - 1] = false;
+        questions[position].close();
+        answers[position].close();
     }
 }
 
