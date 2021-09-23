8f3b5e92c44e0d33cb7af1f232c5caca1eecabb0
KD2Ex <82110046+KD2Ex@users.noreply.github.com>
2021-09-23 13:12:46 +0300

    Update Task4.cpp

--- Task4.cpp
+++ Task4.cpp
@@ -2,17 +2,22 @@
 #include <fstream>
 using namespace std;
 int main() {
-    ifstream file;
-    char buffer[5];
     string path;
+    cout << "Enter a path to the file" << endl;
     cin >> path;
+    ifstream file;
     file.open(path, ios::binary);
-    file.read(buffer, sizeof(buffer));
-    buffer[4] = 0;
-    if (buffer[0] == (char)-119 && buffer[1] == 'P' && buffer[2] == 'N' && buffer[3] == 'G') {
-        cout << "y";
+    if (file.is_open()) {
+        char buffer[5];
+        file.read(buffer, sizeof(buffer));
+        if (buffer[0] == (char)-119 && buffer[1] == 'P' && buffer[2] == 'N' && buffer[3] == 'G'
+        && path.substr(path.length() - 4,4) == ".png") {
+            cout << "y";
+        } else {
+            cout << "n";
+        }
+        file.close();
     } else {
-        cout << "n";
+        cout << "Invalid path to the file";
     }
-    file.close();
 }
