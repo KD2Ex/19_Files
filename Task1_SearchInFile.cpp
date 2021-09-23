058dcf668fc6f9177df634dae37a53294d1ec741
KD2Ex <82110046+KD2Ex@users.noreply.github.com>
2021-09-23 12:53:28 +0300

    Update Task1_SearchInFile.cpp

--- Task1_SearchInFile.cpp
+++ Task1_SearchInFile.cpp
@@ -3,17 +3,21 @@
 using namespace std;
 
 int main() {
-    string str;
-    cout << "Enter a word to find in text:\n";
-    cin >> str;
     ifstream words;
-    words.open("W:\\CLionProjects\\Skillbox\\19_Files\\words.txt");
-    int count = 0;
-    while (!words.eof()) {
-        string word;
-        words >> word;
-        if (word == str) ++count;
+    words.open("words.txt");
+    if (!words.is_open()) {
+        cout << "Invalid file path" << endl;
+    } else {
+        string str;
+        cout << "Enter a word to find in text:\n";
+        cin >> str;
+        int count = 0;
+        while (!words.eof()) {
+            string word;
+            words >> word;
+            if (word == str) ++count;
+        }
+        cout << "The word appears in the text " << count << " times.";
+        words.close();
     }
-    cout << "The word appears in the text " << count << " times.";
-    words.close();
 }
